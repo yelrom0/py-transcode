3e4e57d30d2635865b184b5fa65efd8cb358a614
yelrom0 <yelrom0@gmail.com>
2024-08-30 23:35:18 +1000

    feat(Run): Add anyio for async

--- run.py
+++ run.py
@@ -3,10 +3,26 @@ run.py
 This is the main entry point for the application.
 """
 
+# Package Imports
+from anyio import run
+
 # Local Imports
-from src.fs import FS
+from src.fs import get_files
+from src.cli import parse_args
+from src.media import transcode_files
+
+
+async def main():
+    # No check for args, as an error is raised if
+    # required args not provided
+    args = parse_args()
+
+    # print(f"Path: {args.path}")
+    # print(f"Recursive: {args.recursive}")
+
+    files = get_files(args.path, args.recursive)
+    await transcode_files(files)
+
 
 if __name__ == "__main__":
-    files = FS.get_files("/Users/yelrom0/Movies")
-    # files = FS.get_files("~/Movies")
-    print(f"Files: {files}")
+    run(main)
