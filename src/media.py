ce908d2141179f26c7084687d6e2a19e80aa3bc1
yelrom0 <yelrom0@gmail.com>
2024-08-31 01:28:25 +1000

    chore(Media): Touch-ups and error handling

--- src/media.py
+++ src/media.py
@@ -7,12 +7,14 @@ Module for media operations, currently using python-ffmpeg.
 # Package Imports
 from ffmpeg import Progress
 from ffmpeg.asyncio import FFmpeg
+from ffmpeg.errors import FFmpegUnsupportedCodec
 
 # Local Imports
 from src.fs import get_out_path, write_file
+from src.meta import FileEncodings
 
 
-async def transcode_file(path: str) -> None:
+async def transcode_file(path: str, codec: str = "librav1e") -> None:
     """
     Transcode a video file to av1 format - **yelrom0**
     Args:
@@ -27,7 +29,7 @@ async def transcode_file(path: str) -> None:
         .input(path)
         .output(
             out_path,
-            {"codec:v": "librav1e"},
+            vcodec=codec,
         )
     )
 
@@ -38,7 +40,16 @@ async def transcode_file(path: str) -> None:
         """
         print(f"Progress: {progress}")
 
-    data = await ffmpeg.execute()
+    try:
+        data = await ffmpeg.execute()
+    except FFmpegUnsupportedCodec:
+        if codec == "libaom-av1":
+            print("Error: libaom-av1 codec is not installed.")
+            return
+        # this exception is thrown if the codec is not installed
+        # I'll fall back to libaom-av1
+        await transcode_file(path, "libaom-av1")
+        return
     await write_file(out_path, data)
     print(f"File {out_path} successfully transcoded")
 
@@ -50,3 +61,15 @@ async def transcode_files(paths: list[str]) -> None:
     """
     for path in paths:
         await transcode_file(path)
+
+
+async def check_file_encoding(path: str) -> FileEncodings:
+    """
+    Check the encoding of a file - **yelrom0**
+    Args:
+        path (str): Path to the video file
+    Returns:
+        FileEncodings: The encoding of the file
+    """
+
+    pass
